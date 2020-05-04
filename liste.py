b4cafeb4e421118d59429e7002c348291871592a
David GILLARD <david.gillard@ac-lille.fr>
2020-05-04 14:57:33 +0200

    ajout de la boucle for pour une liste

--- liste.py
+++ liste.py
@@ -1,2 +1,12 @@
+#!/usr/bin/python3
+# -*- encoding utf8 -*-
+
+# definition d'une liste et affichage d'un film
 movies = ["The Holy Grail", 1975, "the life of Brian", 1979, "The meaning of life", 1983]
 print(movies[2])
+
+
+# définition d'une liste et affiche l'ensemble de la liste par un boucle for
+films = ["The Holy Grail", "the life of Brian", "The meaning of life"]
+for chaque_film in films:
+    print(chaque_film)
