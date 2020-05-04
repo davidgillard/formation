10d0e05fe3b9d6498bcfa3e70c514cc1c5f87871
David GILLARD <david.gillard@ac-lille.fr>
2020-05-04 18:20:40 +0200

    Correction t en T

--- liste.py
+++ liste.py
@@ -2,7 +2,7 @@
 # -*- encoding utf8 -*-
 
 # definition d'une liste et affichage d'un film
-movies = ["The Holy Grail", 1975, "the life of Brian", 1979, "The meaning of life", 1983]
+movies = ["The Holy Grail", 1975, "The life of Brian", 1979, "The meaning of life", 1983]
 print(movies[2])
 
 
@@ -10,3 +10,7 @@ print(movies[2])
 films = ["The Holy Grail", "the life of Brian", "The meaning of life"]
 for chaque_film in films:
     print(chaque_film)
+
+liste_films = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91, ["Graham Chapman",["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+
+print(liste_films[4][1][3])
