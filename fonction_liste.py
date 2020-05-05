c5284d9492c9e251c49bf517ddbd4a737b0b39cd
David GILLARD <david.gillard@ac-lille.fr>
2020-05-05 15:33:34 +0200

    ajout de commentaire sur la fonction

--- fonction_liste.py
+++ fonction_liste.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 #-*- encoding utf8 -*-
+""" Il s'agit d'une fonction qui parcours une liste 
+qui contient plusieurs listes imbriquées"""
 
 liste_films = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91, ["Graham Chapman",["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
 
+
+"""cette fonction prends en argument 'ma_liste' qui 
+peut contenir n'importe quelle type de liste (imbriquées y compris)
+tout les éléments sont fournis dans une liste 
+qui s'affiche à l'écran
+"""
 def print_lol(ma_liste):
     for chaque_film in ma_liste:
         if isinstance(chaque_film, list):
